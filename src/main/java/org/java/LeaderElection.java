d74458ebece449376bc9763e83f40ed4df10ab34
mhaidar <mhaidar@areeba.com>
2026-02-15 19:07:06 +0200

    update the leader election implemented methods documentation

--- src/main/java/org/java/LeaderElection.java
+++ src/main/java/org/java/LeaderElection.java
@@ -17,6 +17,13 @@ public class LeaderElection implements Watcher {
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
 
+    /**
+     * Closes the connection to the ZooKeeper server.
+     * This method attempts to release the ZooKeeper resources
+     * and gracefully shuts down the connection. If the operation
+     * is interrupted, the exception is caught and its stack trace
+     * is printed.
+     */
     public void close(){
         try {
             zooKeeper.close();
@@ -25,16 +32,35 @@ public class LeaderElection implements Watcher {
         }
     }
 
+    /**
+     * Waits indefinitely for a notification to resume execution.
+     *
+     * This method uses a synchronized block to acquire a lock on the ZooKeeper instance,
+     * and the thread executing this method will be blocked until it is notified.
+     * It is typically used to keep the application running until the ZooKeeper
+     * session disconnects or another thread explicitly notifies it.
+     *
+     * @throws InterruptedException if the thread waiting on the ZooKeeper instance
+     *         is interrupted while waiting.
+     */
     public void run() throws InterruptedException {
         synchronized (zooKeeper){
             zooKeeper.wait();
         }
     }
 
+
     /**
-     * This process method will be called by the zookeeper library
+     * Processes the events received from ZooKeeper.
+     * This method is triggered whenever a watched event occurs in the ZooKeeper ensemble,
+     * such as changes in connection state or changes to watched nodes.
      *
-     * @param watchedEvent
+     * The method handles:
+     * - Connection events (None): Logs the connection status and handles disconnection notifications.
+     * - Node-related events (NodeCreated, NodeDeleted, NodeDataChanged, NodeChildrenChanged): Currently, these are ignored.
+     *
+     * @param watchedEvent the event received from ZooKeeper. This contains details about
+     *                     the nature of the event such as the type of event and its state.
      */
     @Override
     public void process(WatchedEvent watchedEvent) {
